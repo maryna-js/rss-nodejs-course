5c21d37b71bfba0ccaff61264b0f8591c00abf07
maryna-js <rastargueva.m@gmail.com>
2020-04-12 23:51:12 +0300

    feat: task 3

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+const logger = require('./resources/logger/logger');
 
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
+app.use('*', logger.requests);
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -23,5 +24,25 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
+
+app.use((err, req, res, next) => {
+  logger.error(`Error: ${err.message}`);
+  res.status(500).send('Not working');
+  next();
+});
+
+process.on('unhandledRejection', message => {
+  logger.error(`Unhandled Rejection : ${message}`);
+});
+
+process.on('uncaughtException', err => {
+  logger.error(`Uncaught Exception: ${err.message}`);
+});
+
+// Crosscheck - расскоментируйте для проверки пункта 3 и 4
+
+// throw Error('Oops!');
+
+// Promise.reject(Error('Oops!'));
 
 module.exports = app;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,30 +1,51 @@
 const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
+const error = require('../logger/logger-error.js');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  return res.json(users.map(User.toResponse));
-});
+router.route('/').get(
+  error(async (req, res) => {
+    const users = await usersService.getAll();
+    return res.json(users.map(User.toResponse));
+  })
+);
 
-router.route('/:id').get(async (req, res) => {
-  const id = await usersService.getUserById(req.params.id);
-  return res.json(User.toResponse(id));
-});
+router.route('/:id').get(
+  error(async (req, res) => {
+    const id = await usersService.getUserById(req.params.id);
+    if (id) {
+      return res.json(User.toResponse(id));
+    }
+    return res.status(404).end();
+  })
+);
 
-router.route('/').post(async (req, res) => {
-  const body = await usersService.createUser(req.body);
-  return res.json(User.toResponse(body));
-});
+router.route('/').post(
+  error(async (req, res) => {
+    const body = await usersService.createUser(req.body);
+    return res.json(User.toResponse(body));
+  })
+);
 
-router.route('/:id').put(async (req, res) => {
-  const user = await usersService.updateUser(req.params.id, req.body);
-  return res.json(User.toResponse(user));
-});
+router.route('/:id').put(
+  error(async (req, res) => {
+    const user = await usersService.updateUser(req.params.id, req.body);
+    if (user) {
+      return res.json(User.toResponse(user));
+    }
+    return res.status(404).end();
+  })
+);
 
-router.route('/:id').delete(async (req, res) => {
-  await usersService.deleteUser(req.params.id);
-  res.status(204).send('');
-});
+router.route('/:id').delete(
+  error(async (req, res) => {
+    const user = await usersService.deleteUser(req.params.id);
+    if (user) {
+      res.status(204).send();
+    } else {
+      res.status(404).end();
+    }
+  })
+);
 
 module.exports = router;
